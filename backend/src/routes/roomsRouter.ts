90ec2d2135c35d0e4b1f62307234ad94efbc8595
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-01-29 10:07:02 +0200

    feat: add Room response type to GET /rooms/:id endpoint

--- backend/src/routes/roomsRouter.ts
+++ backend/src/routes/roomsRouter.ts
@@ -3,42 +3,48 @@ import { Contract, Department, Person, Room, Title } from "../models";
 
 const router = Router();
 
-router.get("/:id", async (req: Request<{ id: string }>, res: Response) => {
-  const room = await Room.findByPk(req.params.id, {
-    attributes: ["id", "name", "area"],
-    include: [
-      {
-        model: Contract,
-        as: "contracts",
-        attributes: ["startDate", "endDate"],
-        include: [
-          {
-            model: Person,
-            as: "person",
-            attributes: ["firstName", "lastName"],
-            include: [
-              {
-                model: Department,
-                as: "department",
-                attributes: ["name"],
-              },
-              {
-                model: Title,
-                as: "title",
-                attributes: ["name"],
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  });
+router.get(
+  "/:id",
+  async (
+    req: Request<{ id: string }>,
+    res: Response,
+  ): Promise<Response<Room>> => {
+    const room = await Room.findByPk(req.params.id, {
+      attributes: ["id", "name", "area"],
+      include: [
+        {
+          model: Contract,
+          as: "contracts",
+          attributes: ["startDate", "endDate"],
+          include: [
+            {
+              model: Person,
+              as: "person",
+              attributes: ["firstName", "lastName"],
+              include: [
+                {
+                  model: Department,
+                  as: "department",
+                  attributes: ["name"],
+                },
+                {
+                  model: Title,
+                  as: "title",
+                  attributes: ["name"],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
 
-  if (!room) {
-    return res.json({ error: "Room not found." });
-  }
+    if (!room) {
+      return res.json({ error: "Room not found." });
+    }
 
-  return res.json(room);
-});
+    return res.json(room);
+  },
+);
 
 export default router;
