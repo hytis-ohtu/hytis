91ff11973c0bcab58c188da9a9396c0d7ad34310
Ronja <ronja.sonninen@helsinki.fi>
2026-02-08 11:55:10 +0200

    test:create configuration for vitest and coverage and add one simple test

---
+++ frontend/tests/MainView.test.tsx
@@ -0,0 +1,24 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import MainView from "../src/components/MainView.tsx";
+
+vi.mock("../src/contexts/AuthContext", () => ({
+  useAuth: () => ({
+    user: { name: "Test User" },
+    logout: vi.fn(),
+  }),
+}));
+
+vi.mock("../assets/exactum-2.svg?react", () => ({
+  default: () => <svg data-testid="mock-svg" />,
+}));
+
+vi.mock("../src/services/roomsService");
+
+describe("MainView", () => {
+  it("renders without crashing", () => {
+    render(<MainView />);
+    expect(screen.getByText("Test User")).toBeInTheDocument();
+  });
+});
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,18 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   preview: {
-    allowedHosts: ["hytis-ohtuprojekti-staging.ext.ocp-test-0.k8s.it.helsinki.fi"],
+    allowedHosts: [
+      "hytis-ohtuprojekti-staging.ext.ocp-test-0.k8s.it.helsinki.fi",
+    ],
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./tests/testSetup.ts",
   },
 });
