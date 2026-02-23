064658cdccba6613e82925993e438b31d1e347fd
LeoLampio <149969862+LeoLampio@users.noreply.github.com>
2026-02-23 15:56:54 +0200

    refactor: make hook for transform code

--- frontend/src/components/MainView.tsx
+++ frontend/src/components/MainView.tsx
@@ -1,115 +1,15 @@
 import { AnimatePresence } from "motion/react";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Exactum2 from "../assets/exactum-2.svg?react";
 import { useAuth } from "../contexts/AuthContext";
+import { useMapTransform } from "../hooks/useMapTransform";
 import { findRoomById } from "../services/roomsService";
 import type { Room } from "../types";
 import "./MainView.css";
 import RoomDetails from "./RoomDetails";
 
 function MainView() {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const mapRef = useRef<HTMLDivElement>(null);
-
-  const isClicked = useRef<boolean>(false);
-
-  const transformValues = useRef<{
-    startX: number;
-    startY: number;
-    lastX: number;
-    lastY: number;
-    dist: number;
-    scale: number;
-  }>({
-    startX: 0,
-    startY: 0,
-    lastX: 0,
-    lastY: 0,
-    dist: 0,
-    scale: 1,
-  });
-
-  useEffect(() => {
-    if (!mapRef.current || !containerRef.current) return;
-
-    const map = mapRef.current;
-    const container = containerRef.current;
-
-    const sensitivity = 0.05;
-    const maxZoom = 3;
-    const minZoom = 0.5;
-
-    const onMouseDown = (e: MouseEvent) => {
-      isClicked.current = true;
-      transformValues.current.startX = e.clientX;
-      transformValues.current.startY = e.clientY;
-      transformValues.current.dist = 0;
-    };
-
-    const onMouseUp = (e: MouseEvent) => {
-      isClicked.current = false;
-      transformValues.current.lastX = map.offsetLeft;
-      transformValues.current.lastY = map.offsetTop;
-    };
-
-    const onMouseMove = (e: MouseEvent) => {
-      if (!isClicked.current) return;
-
-      const offsetX = e.clientX - transformValues.current.startX;
-      const offsetY = e.clientY - transformValues.current.startY;
-      transformValues.current.dist = Math.sqrt(
-        offsetX * offsetX + offsetY * offsetY,
-      );
-
-      const nextX = offsetX + transformValues.current.lastX;
-      const nextY = offsetY + transformValues.current.lastY;
-
-      map.style.left = `${nextX}px`;
-      map.style.top = `${nextY}px`;
-    };
-
-    const onScroll = (e: WheelEvent) => {
-      const dir = Math.sign(e.deltaY);
-      const speedEqualizer = Math.max(transformValues.current.scale, 1);
-
-      const zoomAmount = -dir * speedEqualizer * sensitivity;
-      let scale = transformValues.current.scale + zoomAmount;
-      if (scale > maxZoom) scale = maxZoom;
-      else if (scale < minZoom) scale = minZoom;
-
-      let xPos = Number(map.style.left.replace("px", ""));
-      let yPos = Number(map.style.top.replace("px", ""));
-      const x = (e.clientX - xPos) / transformValues.current.scale;
-      const y = (e.clientY - yPos) / transformValues.current.scale;
-      xPos = e.clientX - x * scale;
-      yPos = e.clientY - y * scale;
-
-      transformValues.current.lastX = xPos;
-      transformValues.current.lastY = yPos;
-      transformValues.current.scale = scale;
-
-      map.style.left = `${xPos}px`;
-      map.style.top = `${yPos}px`;
-      map.style.scale = `${scale}`;
-    };
-
-    container.addEventListener("mousedown", onMouseDown);
-    container.addEventListener("mouseup", onMouseUp);
-    container.addEventListener("mousemove", onMouseMove);
-    container.addEventListener("mouseleave", onMouseUp);
-    container.addEventListener("wheel", onScroll);
-
-    const cleanup = () => {
-      container.removeEventListener("mousedown", onMouseDown);
-      container.removeEventListener("mouseup", onMouseUp);
-      container.removeEventListener("mousemove", onMouseMove);
-      container.removeEventListener("mouseleave", onMouseUp);
-      container.removeEventListener("wheel", onScroll);
-    };
-
-    return cleanup;
-  }, []);
-
+  const { mapContainer, inputContainer, hasMoved } = useMapTransform();
   const { user, logout } = useAuth();
 
   const [activeRoomId, setActiveRoomId] = useState<string | null>(null);
@@ -139,7 +39,7 @@ function MainView() {
   }
 
   async function handleClick(event: React.MouseEvent<SVGSVGElement>) {
-    if (transformValues.current.dist > 10) return;
+    if (hasMoved.current) return;
 
     if (event.target instanceof SVGElement) {
       const target = event.target.closest("path[data-room]");
@@ -164,8 +64,8 @@ function MainView() {
       </header>
       <div className="wrapper">
         <div className="main-container">
-          <div ref={containerRef} className="click-container">
-            <div ref={mapRef} className="map-container">
+          <div ref={inputContainer} className="click-container">
+            <div ref={mapContainer} className="map-container">
               <Exactum2 className="map" onClick={handleClick} />
             </div>
           </div>
---
+++ frontend/src/hooks/useMapTransform.ts
@@ -0,0 +1,109 @@
+import { useEffect, useRef } from "react";
+
+export function useMapTransform() {
+  const inputContainerRef = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<HTMLDivElement>(null);
+
+  const isClicked = useRef<boolean>(false);
+  const isMapMoved = useRef<boolean>(false);
+
+  const coords = useRef<{
+    startX: number;
+    startY: number;
+    lastX: number;
+    lastY: number;
+  }>({
+    startX: 0,
+    startY: 0,
+    lastX: 0,
+    lastY: 0,
+  });
+  const scale = useRef<number>(1);
+
+  useEffect(() => {
+    if (!mapRef.current || !inputContainerRef.current) return;
+
+    const map = mapRef.current;
+    const container = inputContainerRef.current;
+
+    const sensitivity = 0.05;
+    const maxZoom = 3;
+    const minZoom = 0.5;
+
+    const onMouseDown = (e: MouseEvent) => {
+      isClicked.current = true;
+      isMapMoved.current = false;
+      coords.current.startX = e.clientX;
+      coords.current.startY = e.clientY;
+    };
+
+    const onMouseUp = (e: MouseEvent) => {
+      isClicked.current = false;
+      coords.current.lastX = map.offsetLeft;
+      coords.current.lastY = map.offsetTop;
+    };
+
+    const onMouseMove = (e: MouseEvent) => {
+      if (!isClicked.current) return;
+
+      const offsetX = e.clientX - coords.current.startX;
+      const offsetY = e.clientY - coords.current.startY;
+
+      if (Math.abs(offsetX) > 0 || Math.abs(offsetY) > 0)
+        isMapMoved.current = true;
+
+      const nextX = offsetX + coords.current.lastX;
+      const nextY = offsetY + coords.current.lastY;
+
+      map.style.left = `${nextX}px`;
+      map.style.top = `${nextY}px`;
+    };
+
+    const onScroll = (e: WheelEvent) => {
+      const dir = Math.sign(e.deltaY);
+      const speedEqualizer = Math.max(scale.current, 1);
+
+      const zoomAmount = -dir * speedEqualizer * sensitivity;
+      let newScale = scale.current + zoomAmount;
+
+      if (newScale > maxZoom) newScale = maxZoom;
+      else if (newScale < minZoom) newScale = minZoom;
+
+      let xPos = Number(map.style.left.replace("px", ""));
+      let yPos = Number(map.style.top.replace("px", ""));
+      const x = (e.clientX - xPos) / scale.current;
+      const y = (e.clientY - yPos) / scale.current;
+      xPos = e.clientX - x * newScale;
+      yPos = e.clientY - y * newScale;
+
+      coords.current.lastX = xPos;
+      coords.current.lastY = yPos;
+      scale.current = newScale;
+
+      map.style.left = `${xPos}px`;
+      map.style.top = `${yPos}px`;
+      map.style.scale = `${newScale}`;
+    };
+
+    container.addEventListener("mousedown", onMouseDown);
+    container.addEventListener("mouseup", onMouseUp);
+    container.addEventListener("mousemove", onMouseMove);
+    container.addEventListener("mouseleave", onMouseUp);
+    container.addEventListener("wheel", onScroll);
+
+    const cleanup = () => {
+      container.removeEventListener("mousedown", onMouseDown);
+      container.removeEventListener("mouseup", onMouseUp);
+      container.removeEventListener("mousemove", onMouseMove);
+      container.removeEventListener("mouseleave", onMouseUp);
+      container.removeEventListener("wheel", onScroll);
+    };
+
+    return cleanup;
+  }, []);
+  return {
+    mapContainer: mapRef,
+    inputContainer: inputContainerRef,
+    hasMoved: isMapMoved,
+  };
+}
