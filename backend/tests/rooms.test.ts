8d51a50daa63bc3c34088356f0e989294f814ec6
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-02-24 12:52:08 +0200

    test: add server tests to verify GET /api/rooms returns all rooms

--- backend/tests/rooms.test.ts
+++ backend/tests/rooms.test.ts
@@ -1,5 +1,6 @@
 import supertest from "supertest";
 import app from "../src/app";
+import { rooms } from "../src/data/rooms";
 
 const api = supertest(app);
 
@@ -8,4 +9,9 @@ test("rooms data is returned as json", async () => {
     .get("/api/rooms")
     .expect(200)
     .expect("Content-Type", /application\/json/);
+});
+
+test("all rooms are returned", async () => {
+  const response = await api.get("/api/rooms").expect(200);
+  expect(response.body).toHaveLength(rooms.length);
 });
