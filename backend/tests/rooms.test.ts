6869a7c3e27bca77293db98f953ce5199114f00a
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-02-24 13:06:38 +0200

    test: add server test to verify GET /api/rooms returns correct data

--- backend/tests/rooms.test.ts
+++ backend/tests/rooms.test.ts
@@ -1,5 +1,6 @@
 import supertest from "supertest";
 import app from "../src/app";
+import { contracts } from "../src/data/contracts";
 import { rooms } from "../src/data/rooms";
 
 const api = supertest(app);
@@ -14,4 +15,22 @@ test("rooms data is returned as json", async () => {
 test("all rooms are returned", async () => {
   const response = await api.get("/api/rooms").expect(200);
   expect(response.body).toHaveLength(rooms.length);
+});
+
+test("returns correct room data", async () => {
+  const response = await api.get("/api/rooms").expect(200);
+  const returnedRoom = response.body[0];
+  const expectedRoom = rooms[0];
+  const expectedContracts = contracts.filter(
+    (contract) => contract.roomId === expectedRoom.id,
+  );
+
+  expect(returnedRoom).toBeDefined();
+  expect(returnedRoom.id).toBe(expectedRoom.id);
+  expect(parseFloat(returnedRoom.area)).toBe(expectedRoom.area);
+  expect(returnedRoom.capacity).toBe(expectedRoom.capacity);
+  expect(returnedRoom.contracts).toHaveLength(expectedContracts.length);
+  expect(returnedRoom.contracts).toEqual(
+    expectedContracts.map((contract) => ({ id: contract.id })),
+  );
 });
