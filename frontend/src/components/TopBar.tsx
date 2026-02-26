846797d7825e1d154b0627b4f92049d8e54d2520
MMorko <atos.ikonen@gmail.com>
2026-02-26 19:25:15 +0200

    feat: css root variables, added TopBarMenu component to TopBar

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./TopBar.css";
+import TopBarMenu from "./TopBarMenu";
 
 export interface TopBarAction {
   id: string;
@@ -13,6 +14,8 @@ interface TopBarProps {
 }
 
 function TopBar({ title = "HYTis", actions = [] }: TopBarProps) {
+  const [menuOpen, setMenuOpen] = React.useState(false);
+
   return (
     <header className="topbar">
       <span className="topbar__title">{title}</span>
@@ -27,6 +30,16 @@ function TopBar({ title = "HYTis", actions = [] }: TopBarProps) {
             {action.icon}
           </button>
         ))}
+        <button
+          className="topbar__button topbar__profile"
+          onClick={(e) => {
+            e.stopPropagation();
+            setMenuOpen((v) => !v);
+          }}
+        >
+          JD
+        </button>
+        {menuOpen && <TopBarMenu onClose={() => setMenuOpen(false)} />}
       </div>
     </header>
   );
