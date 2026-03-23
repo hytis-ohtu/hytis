44ef14b745903fe166965907fcccb1c784e22ad2
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-03-23 16:13:46 +0200

    fix: update rooms integration tests to use mock data with IDs

---
+++ backend/tests/mockData.ts
@@ -0,0 +1,27 @@
+import { contracts } from "../src/data/contracts";
+import { departments } from "../src/data/departments";
+import { people } from "../src/data/people";
+import { rooms } from "../src/data/rooms";
+import { titles } from "../src/data/titles";
+
+export const mockPeople = people.map((person, index) => ({
+  ...person,
+  id: index + 1,
+}));
+
+export const mockDepartments = departments.map((dept, index) => ({
+  ...dept,
+  id: index + 1,
+}));
+
+export const mockTitles = titles.map((title, index) => ({
+  ...title,
+  id: index + 1,
+}));
+
+export const mockContracts = contracts.slice(0, 3).map((contract, index) => ({
+  ...contract,
+  id: index + 1,
+}));
+
+export const mockRoom = { ...rooms[0], id: 1 };
