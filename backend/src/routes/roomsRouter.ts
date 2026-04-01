77a85552631587cda9864e5596a92282ec79beff
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-04-01 22:52:06 +0300

    chore: remove duplicate researchGroup include from GET /api/rooms/:id

--- backend/src/routes/roomsRouter.ts
+++ backend/src/routes/roomsRouter.ts
@@ -79,11 +79,6 @@ router.get(
                   attributes: ["id", "name"],
                 },
                 {
-                  model: ResearchGroup,
-                  as: "researchGroup",
-                  attributes: ["name"],
-                },
-                {
                   model: Person,
                   as: "supervisors",
                   attributes: ["id", "firstName", "lastName"],
