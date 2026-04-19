71d9df270c0e04461a0cd5430b84f52e324b3ab8
Touko Toivonen <touko.t.toivonen@gmail.com>
2026-04-19 12:24:42 +0300

    rm reset button

--- frontend/src/components/ZoomButtons.tsx
+++ frontend/src/components/ZoomButtons.tsx
@@ -1,32 +1,25 @@
-import { ZoomIn, ZoomOut } from "lucide-react";
+import { Minus, Plus } from "lucide-react";
 import "./ZoomButtons.css";
 
 interface ZoomButtonsProps {
   handleZoom: (e: React.MouseEvent, dir: number) => void;
-  handleReset: (e: React.MouseEvent) => void;
 }
 
-function ZoomButtons({ handleZoom, handleReset }: ZoomButtonsProps) {
+function ZoomButtons({ handleZoom }: ZoomButtonsProps) {
   return (
     <div className="zoom-buttons">
       <button
         data-testid="zoom-increase-button"
         onClick={(e) => handleZoom(e, -1)}
       >
-        <ZoomIn size={22} />
+        <Plus size={22} />
       </button>
-      <button
-        data-testid="reset-transform-button"
-        onClick={handleReset}
-        className="zoom-reset-button"
-      >
-        Nollaa
-      </button>
+
       <button
         data-testid="zoom-decrease-button"
         onClick={(e) => handleZoom(e, 1)}
       >
-        <ZoomOut size={22} />
+        <Minus size={22} />
       </button>
     </div>
   );
