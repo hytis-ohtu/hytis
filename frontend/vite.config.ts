1b6803df2bb22300dcb396685c48b16e3aa7816c
MMorko <atos.ikonen@gmail.com>
2026-04-25 18:55:32 +0300

    refactor: added assets to config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
       "@types": path.resolve(__dirname, "./src/types.ts"),
       "@services": path.resolve(__dirname, "./src/services"),
       "@contexts": path.resolve(__dirname, "./src/contexts"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
 
